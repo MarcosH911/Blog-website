2579840003c351a0fb33d5f2f8f40a0194570213
Marcos Hernanz <marcosh.craft@gmail.com>
2022-05-10 17:40:28 +0200

    Store all posts in posts object

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const posts = [];
+
 app.get("/", function (req, res) {
   res.render("home", { homeContent: homeContent });
 });
@@ -37,6 +39,7 @@ app.post("/compose", function (req, res) {
     title: req.body.postTitle,
     body: req.body.postBody,
   };
+  posts.push(post);
 });
 
 app.listen(3000, function () {
